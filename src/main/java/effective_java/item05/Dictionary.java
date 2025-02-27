62e9b716d06b0e551db6b2bee7cfa67113551324
now <guswp1450@naver.com>
2025-02-28 00:36:11 +0900

    effectiveJava item05

---
+++ src/main/java/effective_java/item05/Dictionary.java
@@ -0,0 +1,10 @@
+package effective_java.item05;
+
+public class Dictionary {
+
+    private String str;
+
+    public boolean contains(String word) {
+        return str.contains(word);
+    }
+}
---
+++ src/main/java/effective_java/item05/di/Dictionary.java
@@ -0,0 +1,6 @@
+package effective_java.item05.di;
+
+public interface Dictionary {
+
+    boolean contains(String word);
+}
