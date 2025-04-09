37e76b00cc08a5dbec0cd06348a03f106b545099
now <guswp1450@naver.com>
2025-04-10 00:37:47 +0900

    이펙티브 자바 아이템15

---
+++ src/main/java/effective_java/item15/BadExample.java
@@ -0,0 +1,18 @@
+package effective_java.item15;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BadExample {
+    public static final List<String> NAMES = new ArrayList<>();
+
+    public static final
+
+
+    public static final String HI = "HI";
+
+    public static List<String> getNames() {
+        return NAMES;
+    }
+}
