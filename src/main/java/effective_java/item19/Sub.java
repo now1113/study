aaacf1fbb619db9368ddb789911c01f55a8afecf
now <guswp1450@naver.com>
2025-05-07 16:48:19 +0900

    이펙티브자바 item 19

---
+++ src/main/java/effective_java/item19/Sub.java
@@ -0,0 +1,15 @@
+package effective_java.item19;
+
+public final class Sub extends Super{
+
+    private final String message;
+
+    public Sub() {
+        this.message = "Hello";
+    }
+
+    @Override
+    public void overrideMe() {
+        System.out.println("message = " + message);
+    }
+}
---
+++ src/main/java/effective_java/item19/Super.java
@@ -0,0 +1,12 @@
+package effective_java.item19;
+
+public class Super {
+
+    public Super() {
+        overrideMe();
+    }
+
+    public void overrideMe() {
+
+    }
+}
