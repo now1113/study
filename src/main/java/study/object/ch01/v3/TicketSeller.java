4bcbf9e4db7b61dbeae45e064588be5f1259d795
now <guswp1450@naver.com>
2025-07-22 01:35:51 +0900

    오브젝트 1장

---
+++ src/main/java/study/object/ch01/v3/TicketSeller.java
@@ -0,0 +1,13 @@
+package study.object.ch01.v3;
+
+public class TicketSeller {
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public void sellTo(Audience audience) {
+        ticketOffice.sellTicketTo(audience);
+    }
+}
